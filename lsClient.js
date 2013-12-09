9a2e6a47cf8f3f8c7c134fef5ef63c049d0533e3
marcotesta <mrc.tst@gmail.com>
2013-12-09 18:44:35 +0100

    Fix connection

--- lsClient.js
+++ lsClient.js
@@ -1,7 +1,7 @@
 define(["LightstreamerClient", "StatusWidget"], function(LightstreamerClient, StatusWidget) {
 
-  var hostToUse = document.location.protocol == "file:" ? "http://localhost:8080" : document.location.protocol+"//"+document.location.hostname+(document.location.port?":"+document.location.port:"");
-  var lsClient= new LightstreamerClient(hostToUse, "MARIO");
+  var protocolToUse = document.location.protocol != "file:" ? document.location.protocol : "http:";
+  var lsClient = new LightstreamerClient(protocolToUse+"//localhost:8080","MARIO");
 
   lsClient.addListener(new StatusWidget("left", "10px", true));
 
